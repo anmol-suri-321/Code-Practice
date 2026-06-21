3132b59cd591005d0f5326fd69e654ff0229aebb
Anmol Suri <anmolsuri321@gmail.com>
2026-06-21 21:01:09 +0530

    Initial basis entity classes created

---
+++ LLD/LLDQuestions/JiraTicketSystem/Priority.java
@@ -0,0 +1,7 @@
+package LLD.LLDQuestions.JiraTicketSystem;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ LLD/LLDQuestions/JiraTicketSystem/TicketStatus.java
@@ -0,0 +1,7 @@
+package LLD.LLDQuestions.JiraTicketSystem;
+
+public enum TicketStatus {
+    OPEN,
+    CLOSED,
+    IN_PROGRESS
+}
